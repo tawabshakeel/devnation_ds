7acbbb8f45943e640e2c38f9321f616e4adfa732
WaqasMajeed2121 <engr.waqasmajeed@gmail.com>
2021-07-09 21:57:57 +0500

    My Effort

---
+++ 2021_07_09/Q.py
@@ -0,0 +1,13 @@
+
+
+def Get_Data_From_Dict(My_dict):
+    for item in My_dict:
+        if My_dict[item] == My_dict[item]:
+            return item
+        else:
+            return "Not Exist"
+
+
+my_dict={1:
+             {'Name': 'Waqas', 'Address': 'HYderabad'
+}}
